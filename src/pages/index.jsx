e5c29cec9ee49cd349b43b38bda8c27b473c411d
7Demon <dickyramadhan543@gmail.com>
2026-02-28 21:04:02 +0700

    update eror dedploy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import FloatingOrb from '../components/floatingOrb'
-import Navbar from "../components/NavBar.jsx";
+import Navbar from "../components/Navbar.jsx";
 import Home from "../components/home.jsx";
 
 const Index = () => {
